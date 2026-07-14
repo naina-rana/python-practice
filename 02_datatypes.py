39fb3a9e99961b4a0fab5ffc2f74b82302fd22e2
naina-rana <nainarana2026@gmail.com>
2026-07-14 18:43:15 +0530

    Create 02_datatypes.py

---
+++ 02_datatypes.py
@@ -0,0 +1,10 @@
+# Data Types in Python
+name = "Naina"       # String
+age = 21             # Integer
+height = 5.2         # Float
+is_student = True    # Boolen
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
