815055943f63456f60bcd812d29a0a1bc101bf4b
naina-rana <nainarana2026@gmail.com>
2026-07-14 18:37:42 +0530

    Update 01_variables.py

--- 01_variables.py
+++ 01_variables.py
@@ -1,3 +1,13 @@
-print("Hello , GitHub!")
 name = "Naina"
-print("Welcome",name)
+age = 21
+height = 5.2
+is_student = True
+
+print("Name : ",name)
+print("Age : ",age)
+print("Height : ",height)
+print("Student : ",is_student)
+
+# Updating a variable
+age = 22
+print("Updated Age : ",age)
