f825cf8736fa555fc5680d3521a42aa0f498041c
naina-rana <nainarana2026@gmail.com>
2026-07-14 19:18:48 +0530

    Create 08_string_methods.py

---
+++ 08_string_methods.py
@@ -0,0 +1,9 @@
+# String Methods
+text = "python programming"
+
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.replace("python","Python"))
+print(text.startswith("python"))
+print(text.endswith("ing"))
